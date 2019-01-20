04f5b433d06f2c7fdcc6916de57d4a1ff54db4f0
Ilver Anache <ilver.anache@gmail.com>
2019-01-19 23:58:40 -0500

    ajustes a generacion del ambiente

--- main.tf
+++ main.tf
@@ -1,21 +1,37 @@
+# variables
+variable "region" {
+  default = "RegionOne"
+}
+
+variable "os_user" { default = "admin"  }
+variable "os_pwd" { }
+variable "os_tenant" { default = "admin" }
+variable "network_cidr" {}
+
 # Configure the OpenStack Provider
 provider "openstack" {
-   user_name = "admin"
-   password = "secret"
-   region = "RegionOne"
+   user_name = "${var.os_user}"
+   password = "${var.os_pwd}"
+   region = "${var.region}"
    auth_url = "http://172.17.8.191/identity"
-   tenant_name = "admin"
+   tenant_name = "${var.os_tenant}"
 }
+
+##
+## KeyPair
+##
+resource "openstack_compute_keypair_v2" "fuseesb" {
+  name = "fusesb-keypair"
+}
+
 
 ##
 ## VOLUME
 ##
 resource "openstack_blockstorage_volume_v2" "fuseesb01_myvol" {
   name = "fuseesb01_myvol"
-  size = 2
+  size = 1
   description = "Volumen del servidor FuseESB"
-  region = "RegionOne"
-  count = 1
 }
 
 ##
@@ -29,7 +45,7 @@ resource "openstack_networking_network_v2" "prod_fuse_network" {
 resource "openstack_networking_subnet_v2" "prod_fuse_subnet_1" {
   name       = "subnet_1"
   network_id = "${openstack_networking_network_v2.prod_fuse_network.id}"
-  cidr       = "10.0.2.0/24"
+  cidr       = "${var.network_cidr}"
   ip_version = 4
 }
 
@@ -38,7 +54,7 @@ resource "openstack_networking_subnet_v2" "prod_fuse_subnet_1" {
 ##
 resource "openstack_compute_instance_v2" "fuseesb01" {
   name      = "fuseesb01"
-#  region = "nova"
+  key_pair = "fusesb-keypair"
   flavor_name = "m1.tiny" 
   image_id = "3bc4a056-e530-487a-a04e-1f50fbb12f97"
 
